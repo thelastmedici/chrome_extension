7b8d6d98d5b9f4b89fbb9f24baf1b5cf992a0ba6
user.name: "thelastmedici <user.email: "christianopeyemijoshua@gmail.com>
2023-09-19 09:45:46 +0100

    added more buttons and functionality to the javascript

---
+++ index.js
@@ -0,0 +1,60 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItems = ""
+
+    for (let i = 0; i < leads.length; i++) {
+        "" + leads[i] + ""
+        listItems += `
+        <li> 
+                <a href = ' ${leads[i]}' target='_blank'>
+                ${leads[i]} 
+                </a>
+          </li>
+        
+        `
+        
+        
+    }
+
+    ulEl.innerHTML = listItems
+}
+const tabs = [
+    {url: "https://www.linkedin.com/in/christian-joshua/"},
+]
+
+tabBtn.addEventListener("click", function(){
+    //console.log(JSON.stringify(tabs))
+    console.log(tabs[0])
+
+})
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+   
+
+    inputEl.value = " "
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+    render(myLeads)
+})
+
